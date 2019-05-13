fe0ec2a710e5fdd5371713d0c0f097fe4472a139
Hamza Yousaf <Hamzayousaf933@gmail.com>
2019-05-13 13:14:39 +0500

    auth done login signup save token refresh token

--- app/store/reducers/user_reducer.js
+++ app/store/reducers/user_reducer.js
@@ -1,28 +1,40 @@
 import {
-  SIGN_UP,
-  SIGN_IN,
-  AUTO_SIGN_IN
+    SIGN_UP,
+    SIGN_IN,
+    AUTO_SIGN_IN
 } from '../types';
 
+
 export default function(state={},action){
-  switch(action.type){
-    case SIGN_IN:
-      return {
-        ...state,
-        auth:{
-          email:action.payload.email || false,
-          token:action.payload.email || false,
-        }
-      }
-      case SIGN_UP:
-      return {
-        ...state,
-        auth:{
-          email:action.payload.email || false,
-          token:action.payload.email || false,
-        }
-      }
-    default:
-      return state
-  }
+    switch(action.type){
+        case SIGN_IN:
+            return {
+                ...state,
+                auth:{
+                    uid:action.payload.localId || false,
+                    token:action.payload.idToken || false,
+                    refToken: action.payload.refreshToken || false,
+                }
+            }
+        case SIGN_UP:
+            return {
+                ...state,
+                auth:{
+                   uid:action.payload.localId || false,
+                   token:action.payload.idToken || false,
+                   refToken: action.payload.refreshToken || false,
+                }
+            }
+        case AUTO_SIGN_IN:
+            return {
+                ...state,
+                auth:{
+                    uid:action.payload.user_id || false,
+                    token:action.payload.id_token || false,
+                    refToken: action.payload.refresh_token || false,
+                }
+            }
+        default:
+            return state
+    }
 }
